6eaf536ecfca27e01ea9e0c203a4f326380bd52a
AlmondRumble11 <jess.mustonen@gmail.com>
2022-11-11 13:02:07 +0200

    Navbar works almost...

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -8,9 +8,8 @@ import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 import MenuItem from '@mui/material/MenuItem';
-import { Link as RouterLink} from 'react-router-dom';
+import { Link as RouterLink, useNavigate } from 'react-router-dom';
 import { Button } from '@mui/material';
-import { useNavigate } from "react-router-dom";
 import SearchIcon from '@mui/icons-material/Search';
 import { styled, alpha } from '@mui/material/styles';
 import InputBase from '@mui/material/InputBase';
@@ -35,7 +34,12 @@ const ResponsiveAppBar = () => {
     const handleCloseNavMenu = () => {
         setAnchorElNav(null);
     };
-
+    const changeLanguage = (lang) => {
+        console.log(lang);
+        handleCloseNavMenu();
+     
+      
+    };
     //When user log outs they are redirected to the login page
     const logOut = () => {
         console.log("Log out");
@@ -95,8 +99,20 @@ const ResponsiveAppBar = () => {
             <Toolbar disableGutters> {/* Gutters are set to false, so there wont be spaces between content's tracks*/}
             {/* Name of the site and setting the first box to have flexgrow 1 so all the other components that come after it are on the right side*/}
             <Box display='flex' flexGrow='1'>
-                <Typography variant="h6" noWrap component="div" sx={{ mr: 4}}>
-                    STACK UNDERFLOW
+                <Typography  variant="h6"
+                    noWrap
+                    component={RouterLink}
+                    to='/'
+                    sx={{
+                    mr: 2,
+                    display: { xs: 'none', md: 'flex' },
+                    fontWeight: 700,
+                    letterSpacing: '.3rem',
+                    color: 'inherit',
+                    textDecoration: 'none',
+                    }}
+                >
+                    NordicBooks
                 </Typography>
             </Box>
             <Search>
@@ -115,51 +131,83 @@ const ResponsiveAppBar = () => {
                     <MenuIcon />
                 </IconButton>
                 
-                <Menu id="menu-appbar" anchorEl={anchorElNav} anchorOrigin={{vertical: 'bottom', horizontal: 'left',}} keepMounted transformOrigin={{ vertical: 'top', horizontal: 'left'}} open={Boolean(anchorElNav)} onClose={handleCloseNavMenu} sx={{ display: { xs: 'block', md: 'none' }}}>
+                <Menu 
+                    id="menu-appbar" 
+                    anchorEl={anchorElNav} 
+                    anchorOrigin={{vertical: 'bottom', horizontal: 'left',}} 
+                    keepMounted 
+                    transformOrigin={{ vertical: 'top', horizontal: 'left'}} 
+                    open={Boolean(anchorElNav)} 
+                    onClose={handleCloseNavMenu} 
+                    sx={{ display: { xs: 'block', md: 'none' }}}>
                     {/* Listing menu items that are same as the normal desktop navigation*/}
                     <MenuItem component={RouterLink} to='/' color="inherit" style={{padding: '10px'}}>
-                            Home
+                        Home
                     </MenuItem>
-                    <MenuItem component={RouterLink} to='/posts' color="inherit" style={{padding: '10px'}}>
-                            Posts
-                    </MenuItem>
-                    <MenuItem component={RouterLink} to='/createpost' color="inherit" style={{padding: '10px'}}>
-                            Create a Post
-                    </MenuItem>
-                    <MenuItem component={RouterLink} to='/profile' color="inherit" style={{padding: '10px'}}>
-                            Profile
+                    <MenuItem component={RouterLink} to='/allbooks' color="inherit" style={{padding: '10px'}}>
+                            Allbooks
                     </MenuItem>
                     <MenuItem component={RouterLink} to='/login' color="inherit" style={{padding: '10px'}}>
                             Login
                     </MenuItem>
+                    <MenuItem component={RouterLink} to='/profile' color="inherit" style={{padding: '10px'}}>
+                            Profile
+                    </MenuItem>
                     <MenuItem component={RouterLink} to='/register' color="inherit" style={{padding: '10px'}}>
                             Register
                     </MenuItem>
-                    <Button sx={{color:"red"}} onClick={logOut}>
-                        Logout
-                    </Button>
+                    <MenuItem component={RouterLink} to='/checkout' color="inherit" style={{padding: '10px'}}>
+                            Checkout
+                    </MenuItem>
+                    <Button sx={{color:"inherit"}} onClick={() => {changeLanguage("EN")}}>
+                    EN
+                </Button>
+                <Button sx={{color:"inherit"}} onClick={() => {changeLanguage("SWE")}}>
+                    SWE
+                </Button>
+                <Button sx={{color:"inherit"}} onClick={() => {changeLanguage("NO")}}>
+                    NO
+                </Button>
+                <Button sx={{color:"inherit"}} onClick={() => {changeLanguage("FI")}}>
+                    FI
+                </Button>
+                <Button sx={{color:"red"}} onClick={logOut}>
+                    Logout
+                </Button>
                 </Menu>
             </Box>
             {/* Creating links to pages. Are shown in the desktop mode due to md beign flex and xs none meaning on extra small screens this is hidden*/}
             <Box  sx={{ display: { xs: 'none', md: 'flex' } }}>
-            <MenuItem component={RouterLink} to='/' color="inherit" style={{padding: '10px'}}>
+                <MenuItem component={RouterLink} to='/' color="inherit" style={{padding: '10px'}}onClick={() => {handleCloseNavMenu()}}>
                         Home
                 </MenuItem>
-                <MenuItem component={RouterLink} to='/allbooks' color="inherit" style={{padding: '10px'}}>
+                <MenuItem component={RouterLink} to='/allbooks' color="inherit" style={{padding: '10px'}} onClick={() => {handleCloseNavMenu()}}>
                         Allbooks
                 </MenuItem>
-                <MenuItem component={RouterLink} to='/login' color="inherit" style={{padding: '10px'}}>
+                <MenuItem component={RouterLink} to='/login' color="inherit" style={{padding: '10px'}} onClick={() => {handleCloseNavMenu()}}>
                         Login
                 </MenuItem>
-                <MenuItem component={RouterLink} to='/profile' color="inherit" style={{padding: '10px'}}>
+                <MenuItem component={RouterLink} to='/profile' color="inherit" style={{padding: '10px'}} onClick={() => {handleCloseNavMenu()}}>
                         Profile
                 </MenuItem>
-                <MenuItem component={RouterLink} to='/register' color="inherit" style={{padding: '10px'}}>
+                <MenuItem component={RouterLink} to='/register' color="inherit" style={{padding: '10px'}}  onClick={() => {handleCloseNavMenu()}}>
                         Register
                 </MenuItem>
-                <MenuItem component={RouterLink} to='/checkout' color="inherit" style={{padding: '10px'}}>
+                <MenuItem component={RouterLink} to='/checkout' color="inherit" style={{padding: '10px'}} onClick={() => {handleCloseNavMenu()}}>
                         Checkout
                 </MenuItem>
+                <Button sx={{color:"inherit"}} onClick={() => {changeLanguage("EN")}}>
+                    EN
+                </Button>
+                <Button sx={{color:"inherit"}} onClick={() => {changeLanguage("SWE")}}>
+                    SWE
+                </Button>
+                <Button sx={{color:"inherit"}} onClick={() => {changeLanguage("NO")}}>
+                    NO
+                </Button>
+                <Button sx={{color:"inherit"}} onClick={() => {changeLanguage("FI")}}>
+                    FI
+                </Button>
                 <Button sx={{color:"red"}} onClick={logOut}>
                     Logout
                 </Button>
