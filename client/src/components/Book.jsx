93d1b1f2d02e477cdd53703189b732fd19d91d89
Jezerig <jesse_pasanen@hotmail.com>
2022-11-18 18:23:22 +0200

    Edit home

--- client/src/components/Book.jsx
+++ client/src/components/Book.jsx
@@ -1,12 +1,33 @@
 import React from 'react'
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+
 
 //Displays single book on '/home'
 function Book({book}) {
+    const card = (
+        <React.Fragment>
+          <CardContent>
+            <Typography variant="h5" component="div">
+                {book.Title}
+            </Typography>
+            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                {book.AuthorId}
+            </Typography>
+            <Typography variant="body2">
+              {book.Price}€
+            </Typography>
+          </CardContent>
+        </React.Fragment>
+      );
+    
     return (
         <div>
-            <p>{book.Title}</p>
-            <p>{book.AuthorId}</p>
-            <p>{book.Price}</p>
+            <Box sx={{ width: '75%', margin: 'auto'}}>
+                <Card variant="outlined">{card}</Card>
+            </Box>
         </div>
     )
 }
