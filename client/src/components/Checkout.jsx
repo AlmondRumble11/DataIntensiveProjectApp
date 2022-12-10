e530bbd880a95a41f9b9a2ef47f06abdd57d25be
Joona Heinikari <joona.heinikari@elisanet.fi>
2022-12-10 15:56:34 +0200

    added transaction to checkout UI

--- client/src/components/Checkout.jsx
+++ client/src/components/Checkout.jsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import { useShoppingCart } from '../context/shoppingCartContext';
-import { Box, Card, Typography, CardContent, Button } from '@mui/material';
+import { Box, Card, Typography, CardContent, Button, Divider } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 
 
@@ -16,20 +16,41 @@ export default function Checkout() {
         const newItems = shoppingCart.items.filter(item => item.Id !== bookIdToRemove);
         shoppingCart.setItems(newItems);
     }
+
+    const checkout = () => {
+
+        // TODO transaction
+
+        shoppingCart.setItems([]);
+    }
+
+    const canCheckout = books?.length > 0;
+
     return (
         <div>
-
             <Box sx={{ border: 0, width: '60%', margin: 'auto' }}>
                 <h1 align='left'>{t('Checkout')}</h1>
             </Box>
+
             {books?.map((book) => (
                 <ShoppingCartItem key={book.Id} book={book} removeItem={removeItem} />
             ))}
-            {!books?.length > 0 && <div>{t('EmptyCart')}</div>}
+            {!canCheckout && <div style={{ margin: '1rem' }}>{t('EmptyCart')}</div>}
+
+            <Divider />
+
+            <Box sx={{ border: 0, width: '60%', margin: 'auto', padding: '1rem' }}>
+                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                    <p>
+                        Yhteensä: {books?.reduce((totalPrice, book) => totalPrice + book.Price, 0)}€
+                    </p>
+                    <div style={{ margin: 'auto 0' }}>
+                        <Button disabled={!canCheckout} onClick={() => checkout()} variant="contained">{t('Checkout')}</Button>
+                    </div>
+                </div>
+            </Box>
         </div>
-
     )
-
 }
 
 
@@ -41,10 +62,10 @@ const ShoppingCartItem = ({ book, removeItem }) => {
                 <CardContent sx={{ borderRadius: 2, border: '1.5px solid black' }}>
                     <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '1rem' }}>
 
-                        <Typography align='left' variant="h4" component="div">
+                        <Typography align='left' variant="h5" component="div">
                             {book.Title}
                         </Typography>
-                        <Typography align='right' variant="h4" component="div">
+                        <Typography align='right' variant="h5" component="div">
                             {book.Price}€
                         </Typography>
                     </div>
